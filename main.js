2872e633bc5134c30ad98861e6d528f49eb7c97c
Maksym Shchepetkov <mama2009113@gmail.com>
2025-10-25 10:42:15 +0300

    update css js test tablet

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
-const btn = document.querySelector('.mob-menu-btn');
-const nav = document.querySelector('.header-nav');
-const logo = document.querySelector('.logo-link');
-const contacts = document.querySelector('.contact-btn');
-const headerCont = document.querySelector('.header-cont');
-const gridCont = document.querySelector('.hero-picture-cont');
-const securityMan = document.querySelector('.hero-second-picture');
+document.addEventListener('DOMContentLoaded', () => {
+  const btn = document.querySelector('.mob-menu-btn');
+  const nav = document.querySelector('.header-nav');
+  const logo = document.querySelector('.logo-link');
+  const contacts = document.querySelector('.contact-btn');
+  const headerCont = document.querySelector('.header-cont');
+  const gridCont = document.querySelector('.hero-picture-cont');
+  const securityMan = document.querySelector('.hero-second-picture');
 
-const toggleMenu = () => {
-  nav.classList.toggle('is-open');
-  headerCont.classList.toggle('is-open');
-  btn.classList.toggle('hidden');
-  logo.classList.toggle('hidden');
-  contacts.classList.toggle('hidden');
-};
+  const toggleMenu = () => {
+    nav.classList.toggle('is-open');
+    headerCont.classList.toggle('is-open');
+    btn.classList.toggle('hidden');
+    logo.classList.toggle('hidden');
+    contacts.classList.toggle('hidden');
+  };
 
-const moveToLastColumn = () => {
-  const style = window.getComputedStyle(gridCont);
-  const columns = style
-    .getPropertyValue('grid-template-columns')
-    .trim()
-    .split(' ').length;
-  securityMan.style.gridColumn = `${columns}`;
-};
+  const moveToLastColumn = () => {
+    const style = window.getComputedStyle(gridCont);
+    const columns = style
+      .getPropertyValue('grid-template-columns')
+      .trim()
+      .split(' ').length;
+    securityMan.style.gridColumn = `${columns}`;
+  };
 
-window.addEventListener('resize', moveToLastColumn);
-btn.addEventListener('click', toggleMenu);
+  window.addEventListener('resize', moveToLastColumn);
+  btn.addEventListener('click', toggleMenu);
 
-moveToLastColumn();
+  moveToLastColumn();
+});
